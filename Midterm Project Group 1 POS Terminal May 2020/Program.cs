3de02fab4800f9cb5c2388d62a37a68a6c1cb4ef
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-05-08 16:36:09 -0400

    Midterm Project init for POS Terminal

---
+++ Midterm Project Group 1 POS Terminal May 2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Midterm_Project_Group_1_POS_Terminal_May_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
