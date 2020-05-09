4fec6822636d209c2b799b720409fa72ec761aab
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-05-09 18:32:42 -0400

    Adding Payment, Credit card, Cash, and check classes

---
+++ Midterm Project Group 1 POS Terminal May 2020/CheckPayment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Midterm_Project_Group_1_POS_Terminal_May_2020
+{
+    class CheckPayment : Payment
+    {
+    }
+}
