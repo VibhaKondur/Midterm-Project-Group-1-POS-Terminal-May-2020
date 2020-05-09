b246e0fe0183bd5d0f616083b62dffe99e277c8a
Tomás <tomasrcalvo@gmail.com>
2020-05-09 18:45:27 -0400

    Pushing ProductView, ProductListView, Box and Cart classes.

---
+++ Midterm Project Group 1 POS Terminal May 2020/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Midterm_Project_Group_1_POS_Terminal_May_2020
+{
+    class Box
+    {
+        //PROPERTIES
+        public Product Product { get; set; }
+        pubic int Quantity { get; set; }
+        //OVERLOAD CONSTRUCTOR
+        public Box(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+        //DEFAULT CONSTRUCTOR
+        public Box() {
+        }
+    }
+}
---
+++ Midterm Project Group 1 POS Terminal May 2020/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Midterm_Project_Group_1_POS_Terminal_May_2020
+{
+     class Cart
+	{
+		//PROPERTIES
+		public List<box> Boxes { get; set; }
+		//OVERLOAD CONSTRUCTOR
+		public Boxes(List<box> boxes)
+		{
+			Boxes = boxes;
+		}
+		//METHODS
+		public void AddBox(Box box)
+		{
+			boxes.Add(box);
+		}
+        public double TotalCart()
+        {
+
+        }
+	}
+}
