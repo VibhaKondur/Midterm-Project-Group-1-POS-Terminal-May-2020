958b2e7062bff2dd14dd62c7a9d5ce0846bf17b5
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-05-11 10:31:09 -0400

    updates to credit card class

--- Midterm Project Group 1 POS Terminal May 2020/CreditCardPayment.cs
+++ Midterm Project Group 1 POS Terminal May 2020/CreditCardPayment.cs
@@ -11,8 +11,6 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
 
         public string Name { get; set; }
 
-        public string CreditCard { get; set;}
-
         public string Brand { get; set; }
 
         public string CreditCardNumber { get; set; }
@@ -30,10 +28,9 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
 
         //overloaded constructor
 
-            public CreditCardPayment(string name, string creditCard, string brand, string creditCardNumber, string expirationDate, int cVV)
+            public CreditCardPayment(string name, string brand, string creditCardNumber, string expirationDate, int cVV)
             {
                 Name = name;
-                CreditCard = creditCard;
                 Brand = brand;
                 CreditCardNumber = creditCardNumber;
                 ExpirationDate = expirationDate;
@@ -51,19 +48,19 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
                 Console.WriteLine($"Enter credit card number:  ");
                 string creditCardNumber = Console.ReadLine();
 
-                Console.WriteLine($"Enter expiration date in format MM/YYYY:   ");
-                DateTime expirationDate = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine($"Enter expiration date in format MM/YY:   ");
+                string expirationDate = Console.ReadLine();
 
-                try
+                Console.WriteLine($"Enter 3 digit code (CVV) on back of card :   ");
+                int cVV = int.Parse(Console.ReadLine());
+                cVV = 0;
+
+            try
                 {
-                    if (expirationDate != "MM/YYYY")
+
+                    if (expirationDate != "MM/YY")
                     {
-                        throw new FormatException("Invalid Input. Please input date as format MM/YYYY");
-
-                        Console.WriteLine($"Enter expiration date in format MM/YY:   ");
-
-                        //DateTime expirationDate = DateTime.Parse(Console.ReadLine());
-                        string expirationDate = Console.ReadLine();
+                        //throw new FormatException("Invalid Input. Please input date as format MM/YY");
                         try
                         {
                             if (expirationDate != "MM/YY")
@@ -77,10 +74,6 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
                             Console.WriteLine(e.Message);
                         }
 
-                        Console.WriteLine($"Enter 3 digit code (CVV) on back of card :   ");
-                        int cVV = int.Parse(Console.ReadLine());
-                        cVV = 0;
-
                         try
                         {
                             if (cVV != 123)
@@ -91,22 +84,21 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
                         catch (FormatException e)
                         {
                             Console.WriteLine(e.Message);
-                        }
-
-
-                        Payment.Add(name, creditCardNumber, expirationDate, cVV);
-
-                        string creditCardResult = name + creditCardNumber + expirationDate.ToString() + cVV;
-                        return creditCardResult;
+                        }                   
 
                     }
 
-                }catch
-                    {
-                        Exception;
-                    }
+                }
+                catch
+                     {
+                        new Exception();
+                     }
+            
+    
+            string creditCardResult = name + creditCardNumber + expirationDate.ToString() + cVV;
+            return creditCardResult;
 
-            }
+        }
         //method - using regex - validating type of credit card used for purchase - Visa, MasterCard, Amex, and Discover
 
         public virtual void DetermineCreditCardType(string creditCardNumber)
@@ -120,8 +112,6 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
             if (Regex.IsMatch(creditCardNumber, @"^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$"))
             {
 
-                return CreditCardPayment.masterCard;
-
                 Brand = "Mastercard";
 
             }
@@ -129,15 +119,12 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
             if (Regex.IsMatch(creditCardNumber, @"^3[47][0-9]{13}$"))
             {
 
-                return CreditCardPayment.americanExpress;
-
                 Brand = "Discover";
             }
 
             if (Regex.IsMatch(creditCardNumber, @"^6(?:011|5[0-9]{2})[0-9]{12}$"))
             {
 
-                return CreditCardPayment.discover;
 
                 Brand = "American Express";
 
@@ -149,8 +136,7 @@ namespace Midterm_Project_Group_1_POS_Terminal_May_2020
         //method to validate CVV (security code)
         public bool IsValidSecurityCode(int cVV)
         {
-
-            bool isValid = Regex.Match(cVV, @"^\d{3}$").Success;
+            bool isValid = Regex.Match(cVV.ToString(), @"^\d{3}$").Success;
             return isValid;
         }
 
